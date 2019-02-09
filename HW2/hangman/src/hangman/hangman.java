2c1b7e75a521a3ff3dfbb36ec213c992ea8691c0
Nick Latham <nlatham@zagmail.gonzaga.edu>
2019-02-09 14:40:40 -0800

    added some more stuff

--- HW2/hangman/src/hangman/hangman.java
+++ HW2/hangman/src/hangman/hangman.java
@@ -7,8 +7,10 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class hangman {
-   public static void main(String[] args)
-   {
+    public static void main(String[] args)
+    {
+        char currentLetter = 'a';
+        String unfinishedWord = ""; //string containing blanks and letter correctly guessed
         boolean keepLooping = true;
         while(keepLooping){
             String word = "";
@@ -20,8 +22,60 @@ public class hangman {
             }else{
                 keepLooping = false;
             }
+            
+            unfinishedWord = getUnfinishedWord(word);
+            
+            boolean notDone = true; //switch to loop playing
+            while(notDone){
+                currentLetter = getLetter();
+                unfinishedWord = isLetterInString(word, unfinishedWord, currentLetter);
+                showOutput(unfinishedWord);
+            }
             showOutput(word);
         }
+    }
+    
+    //checks to see if the letter guessed is in the word
+    //if true then it adds it to the unfinished word
+    public static String isLetterInString(String s, String u, char c){
+        int x = s.indexOf(c);
+        StringBuilder word = new StringBuilder(u);
+        
+        if(x > -1){
+             for(int i = 0; i < s.length(); i++){
+                 if(s.charAt(i) == c){
+                     word.setCharAt(i, c);
+                 }
+             }
+        }else{
+            JOptionPane.showMessageDialog(null, "bad choice");
+        }
+        return word.toString();
+    }
+    
+    public static String getUnfinishedWord(String s){
+        String word = "";
+        for(int i = 0; i < s.length(); i++){
+            word += "_";
+        }
+        return word;
+    }
+   
+    public static char getLetter(){
+        boolean notCorrect = true;
+        String input ="";
+        char cInput;
+        while(notCorrect){
+            input = JOptionPane.showInputDialog("enter a character");
+            if(input.length() == 1)
+                notCorrect = false;
+            else{
+                JOptionPane.showMessageDialog(null, "only one character allowed");
+            }
+        }
+        input = input.toLowerCase();
+        cInput = input.charAt(0);
+        return cInput;
    }
 
    //gets the users choice 
